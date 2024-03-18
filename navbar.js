c4c8aa57bc78ad4c392ff0856534de561020daf8
phuchh27 <phuchh27@gmail.com>
2024-03-18 22:11:11 +0700

    hotfix: fix responsive navbar

--- navbar.js
+++ navbar.js
@@ -1,10 +1,22 @@
 const menuButton = document.getElementById("menuButton");
 const mobileMenu = document.getElementById("mobileMenu");
 
+function hideMobileMenuIfNeeded() {
+  if (window.innerWidth > 767) {
+    mobileMenu.style.display = "none";
+  }
+}
+
+hideMobileMenuIfNeeded();
+
 menuButton.addEventListener("click", function () {
-  if (mobileMenu.style.display === "none") {
+  if (mobileMenu.style.display === "none" || mobileMenu.style.display === "") {
     mobileMenu.style.display = "block";
   } else {
     mobileMenu.style.display = "none";
   }
+});
+
+window.addEventListener("resize", function () {
+  hideMobileMenuIfNeeded();
 });
